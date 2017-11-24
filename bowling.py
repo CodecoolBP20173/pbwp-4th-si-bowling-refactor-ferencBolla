413261a6d0cd6930af4402d48f7d7a92ce0e7799
Ferenc Bolla <bollaferenc@gmail.com>
2017-11-24 22:23:00 +0100

    created getResult method which removes deep conditionals

--- bowling.py
+++ bowling.py
@@ -4,19 +4,12 @@ def score(game):
     in_first_half = True
     for i in range(len(game)):
         if game[i] == '/':
-            last = get_value(game[i-1])
+            last = get_value(game[i - 1])
             result += 10 - last
         else:
             result += get_value(game[i])
         if frame < 10 and get_value(game[i]) == 10:
-            if game[i] == '/':
-                result += get_value(game[i + 1])
-            elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i + 1])
-                if game[i + 2] == '/':
-                    result += 10 - get_value(game[i + 1])
-                else:
-                    result += get_value(game[i + 2])
+            result = getResult(game, i, result)
         if not in_first_half:
             frame += 1
         if in_first_half == True:
@@ -26,6 +19,18 @@ def score(game):
         if game[i] == 'X' or game[i] == 'x':
             in_first_half = True
             frame += 1
+    return result
+
+
+def getResult(game, i, result):
+    if game[i] == '/':
+        result += get_value(game[i + 1])
+    elif game[i] == 'X' or game[i] == 'x':
+        result += get_value(game[i + 1])
+        if game[i + 2] == '/':
+            result += 10 - get_value(game[i + 1])
+        else:
+            result += get_value(game[i + 2])
     return result
 
 
